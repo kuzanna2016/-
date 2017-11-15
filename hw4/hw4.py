d017d8ae434bdee155d9e8bba0df445f4b21cebf
kuzanna2016 <31651155+kuzanna2016@users.noreply.github.com>
2017-11-15 11:36:51 +0300

    shishkin les

---
+++ hw4/hw4.py
@@ -0,0 +1,16 @@
+# 3 вариант
+with open("text.txt", encoding="utf-8") as f:
+	text = f.read()
+nopunct = text.replace(",","").replace(".","").replace(")","").replace("(","").replace("'","").replace("!","").replace("?","").replace(":","").replace(";","").replace("-","").replace('"','')
+words = nopunct.split()
+one = 0
+three = 0
+for x in words:
+	if len(x) == 1:
+		one += 1
+	elif len(x) == 3:
+		three += 1
+if one == 0:
+	print("Нет слов длины 1, слов длины 3 - ", three)
+else:
+	print("Слов длины 3 больше чем слов длины 1 в",round(three/one, 2),"раз")
