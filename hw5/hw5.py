c97b783dc70df9f0a1ed6da62c418caf82f4f3d0
kuzanna2016 <31651155+kuzanna2016@users.noreply.github.com>
2017-12-04 23:15:40 +0300

    less is better

--- hw5/hw5.py
+++ hw5/hw5.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # 3 вариант
 with open('text.txt', 'w', encoding='utf-8') as f:
         f.write('')
@@ -15,21 +14,3 @@ while word != '':
     with open('text.txt', 'a', encoding='utf-8') as f:
         f.write(word[count:]+'\n')
     word = input()
-=======
-# 3 вариант
-with open("text.txt", encoding="utf-8") as f:
-	text = f.read()
-nopunct = text.replace(",","").replace(".","").replace(")","").replace("(","").replace("'","").replace("!","").replace("?","").replace(":","").replace(";","").replace("-","").replace('"','')
-words = nopunct.split()
-one = 0
-three = 0
-for x in words:
-	if len(x) == 1:
-		one += 1
-	elif len(x) == 3:
-		three += 1
-if one == 0:
-	print("Нет слов длины 1, слов длины 3 - ", three)
-else:
-	print("Слов длины 3 больше чем слов длины 1 в",round(three/one, 2),"раз")
->>>>>>> origin/master
